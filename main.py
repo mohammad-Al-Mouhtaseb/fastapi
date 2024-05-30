1a8619bfaf4111514f77b9f4d61eb41b0fc9f713
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-05-30 22:09:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
-import requests, scipy, torch, threading
+import requests, scipy, torch
 
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
 processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
@@ -12,18 +12,16 @@ model.to(device)
 sampling_rate = model.config.audio_encoder.sampling_rate
 
 
-def gen_fun(request):
+def gen_fun():
     inputs = processor(
         text=['sad'],
         padding=True,
         return_tensors="pt",
     )
-    audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=1202)
+    audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=100)
     scipy.io.wavfile.write('s1', rate=sampling_rate, data=audio_values[0, 0].cpu().numpy())
 
 @app.get("/")
 async def root():
-    t = threading.Thread(target=gen_fun, args=[request])
-    t.setDaemon(True)
-    t.start()
+    gen_fun()
     return {"res":"sucsess"}
